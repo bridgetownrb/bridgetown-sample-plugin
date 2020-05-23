892e3100b779becc445ac780822c566f71eb3707
Jared White <jared@jaredwhite.com>
2020-05-23 15:23:53 -0700

    Adjust recommended method of loading stylesheet

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,10 +1,9 @@
 /* Use in your app by simply adding to your app's index.js:
+
 import SamplePlugin from "bridgetown-sample-plugin"
 
 SamplePlugin()
 */
-
-import "../styles/index.scss"
 
 export default function() {
   console.log("Success! The sample plugin has been loaded.")
